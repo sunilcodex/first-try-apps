044f87f55a0df32b0a018a13f98100f5db148287
meister.roe <meister.roe@gmail.com>
2013-03-04 15:35:02 +0000

    suche klammer fast fertig

--- MCSever-DynMap/src/net/meisterlabs/mcsever_dynmap/MainActivity.java
+++ MCSever-DynMap/src/net/meisterlabs/mcsever_dynmap/MainActivity.java
@@ -5,6 +5,7 @@ package net.meisterlabs.mcsever_dynmap;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -23,12 +24,11 @@ public class MainActivity extends Activity {
 
 	private static final String LOG_TAG = "WebViewDemo";
 
-    private WebView mWebView;
+    private WebView mWebView1;
 
-    private Handler mHandler = new Handler();
+    private Handler mHandler1 = new Handler();
 
-    @Override
-    public void onCreate(Bundle icicle) {
+    public void onCreate1(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.activity_main);
         mWebView = (WebView) findViewById(R.id.webview);
@@ -43,11 +43,9 @@ public class MainActivity extends Activity {
 
         mWebView.addJavascriptInterface(new DemoJavaScriptInterface(), "demo");
 
-        mWebView.loadUrl("http://merz-server.dyndns.biz:6067/#");
+        mWebView.loadUrl("http://merz-server.dyndns.biz:6067/#  ");
         
-        
-        
-        
+       
         
         
     }
@@ -78,31 +76,88 @@ public class MainActivity extends Activity {
         }
     }
   
-/**public boolean onCreateOptionsMenu(Menu menu) {
-    MenuInflater inflater = getMenuInflater();
-    inflater.inflate(R.menu.activity_main, menu);
-    return true;
-} */
-
-public void info(View view){
-	
-	//info ????
-    WebView mWebView1 = (WebView) findViewById(R.id.w1);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_main, menu);
+        return true;
+    }
 
     
-    mWebView1.setWebChromeClient(new MyWebChromeClient());
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+       
+        switch (item.getItemId()) {
+            case R.id.menu:
+            	setContentView(R.layout.activity_info);
+       
+                return true;
+           
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
-   
+ public void info(View view){
+	
+	private static final String LOG_TAG1 = "WebViewDemo";
 
-    mWebView1.loadUrl("https://googledrive.com/host/0B4aMlHpq7Z9ed05mdmxtc1ZYZ00/serverinfo.html");
-    
-	
-	setContentView(R.layout.activity_info2);
-	
+    private WebView mWebView;
+
+    private Handler mHandler = new Handler();
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.activity_main);
+        mWebView = (WebView) findViewById(R.id.webview);
+
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setSavePassword(false);
+        webSettings.setSaveFormData(false);
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setSupportZoom(false);
+
+        mWebView.setWebChromeClient(new MyWebChromeClient());
+
+        mWebView.addJavascriptInterface(new DemoJavaScriptInterface(), "demo");
+
+        mWebView.loadUrl("https://googledrive.com/host/0B4aMlHpq7Z9ed05mdmxtc1ZYZ00/serverinfo.html");
+        
+       
+        
+        
+    }
+
+    final class DemoJavaScriptInterface1 {
+
+        DemoJavaScriptInterface1() {
+        }
+
+        
+        public void clickOnAndroid() {
+            mHandler.post(new Runnable() {
+                public void run() {
+                    mWebView.loadUrl("javascript:wave()");
+                }
+            });
+        
+        }
+    }
+
+    //chrome
+    final class MyWebChromeClient1 extends WebChromeClient {
+        @Override
+        public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+            Log.d(LOG_TAG, message);
+            result.confirm();
+            return true;
+        }
 	
 	
 }
 
+ 
 
 
 
